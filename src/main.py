dfd7734e65372eb58edd5a7110c8af8b359aa5a9
NathanCo2 <156120309+NathanCo2@users.noreply.github.com>
2024-03-06 11:02:49 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -123,76 +123,35 @@ def Track(shares):
     Task which uses mlx_cam to track target
     Sets setpoint for Aim
     """
-
-    # The following import is only used to check if we have an STM32 board such
-    # as a Pyboard or Nucleo; if not, use a different library
-    try:
-        from pyb import info
-
-    # Oops, it's not an STM32; assume generic machine.I2C for ESP32 and others
-    except ImportError:
-        # For ESP32 38-pin cheapo board from NodeMCU, KeeYees, etc.
-        i2c_bus = I2C(1, scl=Pin(22), sda=Pin(21))
-
-    # OK, we do have an STM32, so just use the default pin assignments for I2C1
-    else:
-        i2c_bus = I2C(1)
-
-    print("MXL90640 Easy(ish) Driver Test")
+    # Initialize I2C bus
+    i2c_bus = I2C(1)
 
     # Select MLX90640 camera I2C address, normally 0x33, and check the bus
     i2c_address = 0x33
-    scanhex = [f"0x{addr:X}" for addr in i2c_bus.scan()]
-    print(f"I2C Scan: {scanhex}")
 
     # Create the camera object and set it up in default mode
     camera = MLX_Cam(i2c_bus)
-    print(f"Current refresh rate: {camera._camera.refresh_rate}")
     camera._camera.refresh_rate = 10.0
-    print(f"Refresh rate is now:  {camera._camera.refresh_rate}")
 
+    # Get image (raw file, nonblocking)
     while True:
         try:
             # Get and image and see how long it takes to grab that image
-            print("Click.", end='')
+            # print("Click.", end='')
             begintime = time.ticks_ms()
-#             image = camera.get_image()
+            image = camera.get_image()
 
             # Keep trying to get an image; this could be done in a task, with
             # the task yielding repeatedly until an image is available
             image = None
             while not image:
                 image = camera.get_image_nonblocking()
-                time.sleep_ms(50)
+                yield
+            # Full image grabbed, yield image
+            yield image
 
-            print(f" {time.ticks_diff(time.ticks_ms(), begintime)} ms")
-
-            # Can show image.v_ir, image.alpha, or image.buf; image.v_ir best?
-            # Display pixellated grayscale or numbers in CSV format; the CSV
-            # could also be written to a file. Spreadsheets, Matlab(tm), or
-            # CPython can read CSV and make a decent false-color heat plot.
-            show_image = False
-            show_csv = True
-            if show_image:
-                camera.ascii_image(image)
-            elif show_csv:
-                for line in camera.get_csv(image, limits=(0, 99)):
-                    print(line)
-            else:
-                camera.ascii_art(image)
-            gc.collect()
-            print(f"Memory: {gc.mem_free()} B free")
-            time.sleep_ms(3141)
-
-        except KeyboardInterrupt:
-            break
-
-    print ("Done.")
-
-
-    # Initialize Camera
-    # Get image (raw file, nonblocking)
     # Digest data (find column with highest heat signature)
+    
     # Convert to set point (some fancy trig using 55 deg FOV and known table length
     # Get setpoint to Aim task
         
