87a0926ae5b04d820568ed9458e64c77489693d4
jrbanh <156119463+jrbanh@users.noreply.github.com>
2024-03-08 11:53:03 -0800

    servo werk

--- src/Servo.py
+++ src/Servo.py
@@ -1,21 +1,29 @@
 import pyb
 import time
 
-# Initializing pin A0
-pinA1 = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP)
+# Initializing pin A10
+pinA10 = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
 
-# Timer channel 2
-TIM2 = pyb.Timer(2, freq=50)
-ch2 = TIM2.channel(2, pyb.Timer.PWM, pin=pinA1)
+# Timer channel 1
+TIM1 = pyb.Timer(1, freq=50)
+ch3 = TIM1.channel(3, pyb.Timer.PWM, pin=pinA10)
 
 # Function to set duty cycle (0 to 100)
 def set_duty_cycle(duty_cycle):
-    ch2.pulse_width_percent(duty_cycle)
+    ch3.pulse_width_percent(duty_cycle)
 
 if __name__ == "__main__":
-    while True:
-        set_duty_cycle(1)  
-        time.sleep(.5)      
-        set_duty_cycle(6) 
-        time.sleep(.5)      	
+    while True:     
+        set_duty_cycle(.75) 
+        time.sleep(2)
+        print('1')
+        set_duty_cycle(5)  
+        time.sleep(2)
+        print('2')
+        set_duty_cycle(8.75) 
+        time.sleep(2)
+        print('3')
+        set_duty_cycle(5)  
+        time.sleep(3) 
 
+# duty cycle of 5 is 0 degrees
