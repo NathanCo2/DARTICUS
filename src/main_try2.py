da13c1566a51eaecf16b5bece66d580553b95a6b
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-03-10 17:04:58 -0700

    5

--- src/main_try2.py
+++ src/main_try2.py
@@ -1,6 +1,7 @@
 import gc
 import pyb
 import time
+import cqueue
 import cotask
 import task_share
 from motor_driver import MotorDriver
@@ -9,13 +10,17 @@ from motor_controller_4 import MotorController
 from servo_driver import ServoDriver
 
 
-def Aim(shares):
+def Aim(shares, shares2):
     my_share, my_queue = shares
-    counter = 0
-    while True:
-        counter += 1
-        my_share.put(counter)
-        yield 
+    pivot_done, queue2 = shares2
+    
+    check = pivot_done.get()
+    if check == 1:
+        counter = 0
+        while True:
+            counter += 1
+            my_share.put(counter)
+            yield 
 
 def Fire(shares):
     the_share, the_queue = shares
@@ -54,47 +59,44 @@ def Fire(shares):
         
     
 def Pivot(shares2):
-    # Get references to the gain and setpoint which have been passed to this task
-    gain, setpoint = shares2
+    pivot_done, queue1 = shares2
     
-    # Initialize Values
-    counter = 0
-    
-    # Create variables to pass to tasks. queues are for printing data, gain and setpoint are input
-    time1 = cqueue.FloatQueue(200)
-    val1 = cqueue.FloatQueue(200)
-
-    # Initialize motor drivers and encoders
-    # set up timer 8 for encoder 2
-    TIM8 = pyb.Timer(8, prescaler=1, period=0xFFFF) # Timer 8, no prescalar, frequency 100kHz
-    #Define pin assignments for encoder 2
-    pinc6 = pyb.Pin(pyb.Pin.board.PC6)
-    pinc7 = pyb.Pin(pyb.Pin.board.PC7)
-    # Create encoder object
-    Jerry = Encoder(pinc6, pinc7, TIM8)
-    
-    # setup motor
-    TIM5 = pyb.Timer(5, freq=2000) # Timer 5, frequency 2000Hz
-    # Define pin assignments for motor 2
-    pinc1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
-    pina0= pyb.Pin(pyb.Pin.board.PA0)
-    pina1 = pyb.Pin(pyb.Pin.board.PA1)    
-    # Create motor driver
-    Tom = MotorDriver(pinc1, pina0, pina1, TIM5)
-    Jerry.zero()
-    # Create motor controller
-    Deitch = MotorController(gain, setpoint, Tom.set_duty_cycle, Jerry.read, time1, val1)
-    for i in range(200):
-        Deitch.run()
+    if wait == True:
+        # Initialize motor drivers and encoders
+        # set up timer 8 for encoder 2
+        TIM8 = pyb.Timer(8, prescaler=1, period=0xFFFF) # Timer 8, no prescalar, frequency 100kHz
+        #Define pin assignments for encoder 2
+        pinc6 = pyb.Pin(pyb.Pin.board.PC6)
+        pinc7 = pyb.Pin(pyb.Pin.board.PC7)
+        # Create encoder object
+        Jerry = Encoder(pinc6, pinc7, TIM8)
+        
+        # setup motor
+        TIM5 = pyb.Timer(5, freq=2000) # Timer 5, frequency 2000Hz
+        # Define pin assignments for motor 2
+        pinc1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
+        pina0= pyb.Pin(pyb.Pin.board.PA0)
+        pina1 = pyb.Pin(pyb.Pin.board.PA1)    
+        # Create motor driver
+        Tom = MotorDriver(pinc1, pina0, pina1, TIM5)
+        Jerry.zero()
+        # Create motor controller
+        Deitch = MotorController(gain, setpoint, Tom.set_duty_cycle, Jerry.read, time1, val1)
+        for i in range(200):
+            Deitch.run()
+            yield
+        print("done 1")
+        Tom.set_duty_cycle(0)
+        
+        # once done set flag and wait
+        # Initialize Values
+        counter = 0
+        while True:
+            counter += 1
+            pivot_done.put(counter)
+            yield
+    else:
         yield
-    print("done 1")
-    Tom.set_duty_cycle(0)
-    
-    # once done set flag and wait
-    while True:
-        counter += 1
-        my_share2.put(counter)
-        yield 
 
 def task4_fun(shares2):
     the_share2, the_queue2 = shares2
@@ -108,17 +110,24 @@ def task4_fun(shares2):
 #         yield 0
 
 if __name__ == "__main__":
-    print("Testing ME405 stuff in cotask.py and task_share.py\r\n"
+    #Initalize values:
+    waiting = True
+    
+    # Create variables to pass to tasks. queues are for printing data, gain and setpoint are input
+    time1 = cqueue.FloatQueue(200)
+    val1 = cqueue.FloatQueue(200)
+    
+    print("FIRING DARTICUS!!! \n"
           "Press Ctrl-C to stop and show diagnostics.")
 
     share0 = task_share.Share('h', thread_protect=False, name="Share 0")
     share1 = task_share.Share('h', thread_protect=False, name="Share 1")
 
-    task1 = cotask.Task(Aim, name="Aim", priority=4, period=100,
+    task1 = cotask.Task(Aim, name="Aim", priority=1, period=100,
                         profile=True, trace=False, shares=(share0, share1))
     task2 = cotask.Task(Fire, name="Fire", priority=2, period=100,
                         profile=True, trace=False, shares=(share0, share1))
-    task3 = cotask.Task(Pivot, name="Pivot", priority=1, period=200,
+    task3 = cotask.Task(Pivot, name="Pivot", priority=4, period=200,
                         profile=True, trace=False, shares=(share1, share0))
     task4 = cotask.Task(task4_fun, name="Task 4", priority=3, period=200,
                         profile=True, trace=False, shares=(share1, share0))
