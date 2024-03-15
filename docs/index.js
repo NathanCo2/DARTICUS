7961bfcdde91d8cb66d8794e74dbac1107025bcb
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-03-15 14:00:29 -0700

    doxygen update

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,10 @@
 var index =
 [
-    [ "Hardware", "index.html#Hardware", null ]
+    [ "Hardware", "index.html#Hardware", null ],
+    [ "Tasks", "index.html#System", [
+      [ "Pivot", "index.html#Pivot", null ],
+      [ "Aim", "index.html#Aim", null ],
+      [ "Fire", "index.html#Fire", null ],
+      [ "Track", "index.html#Track", null ]
+    ] ]
 ];
