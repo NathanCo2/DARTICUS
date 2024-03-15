ba2ec38510e0f8f25efeffd6613cc6c0e23e24b4
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-03-15 13:12:08 -0700

    doxygen

--- src/main_page.py
+++ src/main_page.py
@@ -1,11 +1,18 @@
 """!
 \mainpage D.A.R.T.I.C.U.S.
-\section Software
+\section 
 \subsection Aim
 \subsection Fire
 \section Hardware
-\image html
 
 
 
 """
+
+# \subsubsection sub1 State 1
+# to use same name later just need
+# \subsubsection sub 1
+# \image html imagename.png/.jpg width=500 "WILL NOT LOAD IMAGE UNTIL DOXY HAS A IMAGE DIRECTROY"
+# \section {section-name} "DO NOT USE SPACES!'
+# \subsection {subsection.name} {Title}
+# \subsubsection {subsection-name} {Title}
